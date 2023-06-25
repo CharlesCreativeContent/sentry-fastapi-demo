69bd18ae2292cc59111537516f45ef3c467b1ec8
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2023-06-25 19:25:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # main.py
 from fastapi import FastAPI, Request
 
+import sentry_sdk
+
+sentry_sdk.init(
+    dsn="https://05e7c2630bbb4cb7b85d45ddac2dffd9@o4505422686584832.ingest.sentry.io/4505422689075200",
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+)
+
 app = FastAPI()
 
 @app.get("/")
