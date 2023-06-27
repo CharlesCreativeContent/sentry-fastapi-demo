450a8748f3835f937fbdf921ce19df1bf4a6e8bd
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2023-06-26 21:12:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,17 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello this will be the home page until i figure out how to display a webpage or redirect"}
-
-@app.get("/pokemon/")
-async def root():
-    return {"message": "Hello pokemon"}
-
-@app.get("/map/")
-async def root():
-    return {"message": "Hello map"}
-
-@app.get("/items/{item_id}")
-def read_root(item_id: str, request: Request):
-    client_host = request.client.host
-    return {"client_host": client_host, "item_id": item_id}
+    return {"message": "output Message here"}
